3d012bfe6729392f39cdb6d08975fa1fe9b65eef
Almond <iwanttobebetter7@gmail.com>
2021-10-29 21:11:20 +0700

    test_hello_world: flake8-ed
    Signed-off-by: Almond

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,4 +1,5 @@
 from AI_intro_project.hello_world import HelloWorld
 
 def test_names():
-    assert HelloWorld().names() == {'Minh', 'Phuc', 'Nam', 'Anh', 'Phong'}, 'Test HelloWorld().names(): return enough names of 5 members'
+    assert HelloWorld().names() == {'Minh', 'Phuc', 'Nam', 'Anh', 'Phong'}, \
+        'Test HelloWorld().names(): return enough names of 5 members'
