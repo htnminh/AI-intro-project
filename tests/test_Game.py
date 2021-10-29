96b4d9453e9038485959a1e2b3f8661c27b3240c
Almond <iwanttobebetter7@gmail.com>
2021-10-29 20:55:14 +0700

    test_Game: for flake8's sake.
    Signed-off-by: Almond

--- tests/test_Game.py
+++ tests/test_Game.py
@@ -1,12 +1,13 @@
-from AI_intro_project.Game import *
+from AI_intro_project.Game import Road
 
 # Initial variable used for testing
 # Assuming board_size is (4, 4)
-coord_rd = (2, 3) # a totally-not-random coord
-coord_ul = (1, 1) # Up-Left aka. North-West
-coord_dr = (3, 3) # Down-Right aka. South-East
+coord_rd = (2, 3)  # a totally-not-random coord
+coord_ul = (1, 1)  # Up-Left aka. North-West
+coord_dr = (3, 3)  # Down-Right aka. South-East
 coord_ul_invalid = (0, 0)
 coord_dr_invalid = (4, 4)
+
 
 #
 # TEST FOR CLASS: AI_intro_project.Game.Road()
@@ -18,7 +19,7 @@ def test_coordinate_end_calc():
 
     assert Road(*(coord_ul + tuple('L'))).coordinate_end == (1, 0), \
         'Test coordinate_end_calc for (1, 1, L) did not return (0, 1)'
-    
+
     assert Road(*(coord_dr + tuple('D'))).coordinate_end == (4, 3), \
         'Test coordinate_end_calc for (3, 3, D) did not return (4, 3)'
 
@@ -27,13 +28,12 @@ def test_coordinate_end_calc():
 
     # test for corner coords, these should return 'invalid'
     # TODO: impl invalid moves first in main game logic.
+    assert 1 == 1
 
 def test_eq():
     # told you that _rd is not random
-    assert Road(*(coord_dr + tuple('U'))) == \
-           Road(*(coord_rd + tuple('D'))), \
+    assert Road(*(coord_dr + tuple('U'))) == Road(*(coord_rd + tuple('D'))), \
         'Test Road(2, 3, D) == Road(3, 3, U) did not return True'
-    
-    assert Road(*(coord_ul + tuple('D'))) != \
-           Road(*((2, 2) + tuple('L'))), \
+
+    assert Road(*(coord_ul + tuple('D'))) != Road(*((2, 2) + tuple('L'))), \
         'Test Road(1, 1, D) == Road(2, 2, L) did not return False'
