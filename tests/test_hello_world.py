ea5815b6d75f3f5d4a503d4051110d883ab602b7
Almond <iwanttobebetter7@gmail.com>
2021-10-31 17:20:31 +0700

    test_hello_world: flake8 plus lazyness is annoying
    Signed-off-by: Almond

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,5 +1,6 @@
 from AI_intro_project.hello_world import HelloWorld
 
+
 def test_names():
     assert HelloWorld().names() == {'Minh', 'Phuc', 'Nam', 'Anh', 'Phong'}, \
         'Test HelloWorld().names(): return enough names of 5 members'
