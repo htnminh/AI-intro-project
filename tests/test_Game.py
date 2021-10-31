cc9a966f517881d174adae56bf197acc03d32881
Almond <iwanttobebetter7@gmail.com>
2021-10-31 16:17:16 +0700

    test_Game: add tests for Coordinate and naming convention
    test_coordinate_end_calc
    --> test_road_coordinate_end_calc
    
    Still thinking if Road is strictly better or
    road is fine enough...
    
    Signed-off-by: Almond

--- tests/test_Game.py
+++ tests/test_Game.py
@@ -4,37 +4,57 @@
 from AI_intro_project.Game import Road, Coordinate
 
 # Initial variable used for testing
-# Assuming board_size is (4, 4)
+# Currently assuming board_size is (4, 4)
 coord_rd = (2, 3)  # a totally-not-random coord
 coord_ul = (1, 1)  # Up-Left aka. North-West
 coord_dr = (3, 3)  # Down-Right aka. South-East
-coord_ul_invalid = (0, 0)
-coord_dr_invalid = (4, 4)
+coord_ul_invalid = (-1, -1)
+coord_dr_invalid = (5, 5)
+board_size = (4, 4)
+
+
+#
+# TEST FOR CLASS: AI_intro_project.Game.Coordinate()
+#
+def test_coord_check_inside():
+    # Check coords: 2x invalid & 1 valid
+    assert not Coordinate(*coord_ul_invalid).check_inside(*board_size), \
+        'test Coordinate(-1, -1).check_inside(4, 4) failed'
+
+    assert not Coordinate(*coord_dr_invalid).check_inside(*board_size), \
+        'test Coordinate(5, 5).check_inside(4, 4) failed'
+
+    assert Coordinate(*coord_ul).check_inside(*board_size), \
+        'test Coordinate(1, 1).check_inside(4, 4) failed'
+
+
+def test_coord_eq():
+    assert Coordinate(1, 1) == Coordinate(*coord_ul), \
+        'test Coordinate.__eq__ for == failed'
+
+    assert Coordinate(1, 1) != Coordinate(2, 1), \
+        'test Coordinate.__eq__ for != failed'
 
 
 #
 # TEST FOR CLASS: AI_intro_project.Game.Road()
 #
-def test_coordinate_end_calc():
+def test_road_coordinate_end_calc():
     # test for near-edge coords
     assert Road(*coord_ul, 'U').coordinate_end == Coordinate(0, 1), \
-        'Test coordinate_end_calc for (1, 1, U) did not return (0, 1)'
+        'Test Road.coordinate_end_calc for (1, 1, U) did not return (0, 1)'
 
     assert Road(*coord_ul, 'L').coordinate_end == Coordinate(1, 0), \
-        'Test coordinate_end_calc for (1, 1, L) did not return (0, 1)'
+        'Test Road.coordinate_end_calc for (1, 1, L) did not return (0, 1)'
 
     assert Road(*coord_dr, 'D').coordinate_end == Coordinate(4, 3), \
-        'Test coordinate_end_calc for (3, 3, D) did not return (4, 3)'
+        'Test Road.coordinate_end_calc for (3, 3, D) did not return (4, 3)'
 
     assert Road(*coord_dr, 'R').coordinate_end == Coordinate(3, 4), \
-        'Test coordinate_end_calc for (3, 3, R) did not return (3, 4)'
-
-    # test for corner coords, these should return 'invalid'
-    # TODO: impl invalid moves first in main game logic.
-    assert 1 == 1
+        'Test Road.coordinate_end_calc for (3, 3, R) did not return (3, 4)'
 
 
-def test_eq():
+def test_road_eq():
     # told you that _rd is not random
     assert Road(*coord_dr, 'U') == Road(*(coord_rd + tuple('D'))), \
         'Test Road(2, 3, D) == Road(3, 3, U) did not return True'
