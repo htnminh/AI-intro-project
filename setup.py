abc25c170d55fcc9061411c8abffb0a92be2d3ad
htnminh <nhatminh2106@gmail.com>
2021-11-26 18:46:00 +0700

    add requirements in setup.py, and remove it in workflow

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from distutils.core import setup
-setup(name='AIIntroProject', version='1.0', packages=['AI_intro_project'])
+
+with open('requirements.txt') as f:
+    reqs = f.read().splitlines()
+
+setup(name='AIIntroProject',
+      version='1.0',
+      packages=['AI_intro_project'],
+      install_requires=reqs
+      )
 
 # \AI-intro-project> pip install .
 # \AI-intro-project> pip uninstall AIIntroProject
