f4c6650689442d85d4d1d45d3afaefb826e870fe
Almond <iwanttobebetter7@gmail.com>
2021-12-22 22:11:41 +0700

    search/dfs: time limit and sample output
    the dino can stay
    
    Signed-off-by: Almond

--- AI_intro_project/search_algo/dfs.py
+++ AI_intro_project/search_algo/dfs.py
@@ -1,6 +1,7 @@
 from typing import List
 from AI_intro_project.State import State
 from AI_intro_project.Coordinate_and_Move import Coordinate, Move
+from AI_intro_project._Utilities import _Utilities
 from copy import deepcopy
 import time
 MODE_DBG = False
@@ -45,6 +46,7 @@ def dfs_prep(START: State):
     _START_point = START.current_pos
     _START_tax = START.current_tax
 
+
     #############################################
     # GOAL: where we start our path-finding:
     #       inherit properties from START.
@@ -65,53 +67,79 @@ def dfs_prep(START: State):
     dfs(GOAL)
     return GOAL.path[::-1]
 
+
 #################################################
 # DFS magics
 def dfs(cur):
-    # ABORT signal var
-    global exiting
-    if not exiting and cur.available_moves_list() is not None:        
-        for _move in cur.available_moves_list():
-            # ABORT signal
-            if exiting: break
+    # ABORT: out of time
+    global t_limit_r
+    if t_limit_r: pass
+    elif time.time() - t_start > t_limit:
+        print(f"ABORT: time limit reached: {t_limit}", file = f)
+        t_limit_r = True
+        pass
 
-            # temporary node
-            temp = deepcopy(cur)
+    else:
+        # ABORT signal var
+        global exiting
+        if not exiting and cur.available_moves_list() is not None:
+            for _move in cur.available_moves_list():
+                # ABORT signal
+                if exiting: break
 
-            # and move to that node
-            temp.move_on_move(_move)
-            if cur.current_pos == _START_point:
-                # found path
-                if cur.current_tax >= _START_tax:
-                    # found solution
+                # temporary node
+                temp = deepcopy(cur)
+
+                # and move to that node
+                temp.move_on_move(_move)
+                if cur.current_pos == _START_point:
+                    # found path
+                    if cur.current_tax >= _START_tax:
+                        # found solution
+                        GOAL.path = temp.path
+                        exiting = True
+                        break
+                    
                     GOAL.path = temp.path
-                    exiting = True
-                    break
 
-            _c = _move.coordinate_end_calc()
-            _move = Move(
-                _c.x,
-                _c.y,
-                _move.reverse_direction()
-            )
-            temp.path.append(_move)
+                _c = _move.coordinate_end_calc()
+                _move = Move(
+                    _c.x,
+                    _c.y,
+                    _move.reverse_direction()
+                )
+                temp.path.append(_move)
 
-            dfs(temp)    
+                dfs(temp)    
 
 if __name__ == "__main__":
-    timer = time.time()
+    t_limit = 30 # seconds
 
-    # init
-    _internalVar = State()
-    # _internalVar.initialize_6x6_default()
-    _internalVar.initialize_4x4_default()
+    with open("AI_intro_project/search_algo/load_all_output/output-dfs.txt", "w") as f:
+        for _internalVar in _Utilities().load_all(
+            sizes=[(i,j) for i in range(4,9) for j in range(4,9)],
+            directory='AI_intro_project/randomized_states',
+            extension='state'
+        ):
+            timer = time.time()
 
-    # signal DFS to ABORT: found solution
-    global exiting
-    exiting = False
+            # print basic info
+            print("\nboard size:",_internalVar.board_size, file = f)
+            print(
+                f"start at: ({_internalVar.current_pos.x}, {_internalVar.current_pos.y})",
+                file = f
+            )
+            print("tax:", _internalVar.current_tax, file = f)
 
-    # print
-    print(*dfs_prep(_internalVar), sep = '\n')
+            # signal DFS to ABORT: found solution
+            exiting = False
 
-    timer -= time.time()
-    print(f"time: {-timer}")
+            # time limit
+            t_start = time.time()
+            t_limit_r = False
+
+            # print
+            print(*dfs_prep(_internalVar), sep = '\n', file = f)
+
+            timer -= time.time()
+            print(f"time: {-timer}", file = f)
