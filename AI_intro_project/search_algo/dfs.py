f8b66d824cd1a5d724647d9d8b5a58ded1fdff17
Almond <iwanttobebetter7@gmail.com>
2021-12-24 11:11:05 +0700

    search/DFS: documented DFS search and first-batch output with time_limit 60
    State Solved: 28/50
    State Unsolved by DFS: 21/50
    State Unsolved by problem: 1/50
    
    Signed-off-by: Almond

--- AI_intro_project/search_algo/dfs.py
+++ AI_intro_project/search_algo/dfs.py
@@ -4,67 +4,80 @@ from AI_intro_project.Coordinate_and_Move import Coordinate, Move
 from AI_intro_project._Utilities import _Utilities
 from copy import deepcopy
 import time
+import matplotlib.pyplot as plt
+
 MODE_DBG = False
+if MODE_DBG: print("===\n!DBG: Debug Mode is on\n===")
 
 '''
-	
-                        /|/|/|\\|\|                                       
-                      |  | | | | | |\                                   
-                    | | |  | || || | |\                                  
-                   /  | | |  || || ||||                                
-                   || | | |  | | |  | |\                                
-                 |  | | |  || ||  | | | \                                
-                /  |  | |  ||  | || | ||\                               
-                | | | | | || |  ||   | | |                              
-                | | |    | | || |  | | |||\                             
-               || |  | | | |  | || | | ||| \                           
-              ||  |    |   | | ||| |     | |                            
-              ||| | | |  | | |  || | | ||  |\                           
-              | | | | |  |   | || |||| ||| |\                           
-             /|  |  | | |  |  | | | || | ||  |                            
-             |||| | |__ |__| |__|   || | || ||\                         
-            |_| _ --,   ,.. ,   ,.... _|_|_ || |\                       
-            /,   ,..,    ,.,    ,.. ,   ,..-|_\| |                       
-         _ /.,    ,.,    ,,     ,...   ,...,   .\\                        
-      _ /  ,..,    ,     ,      ,.,    ,.,    _ ...\\\\                 
-    _/       ',,   ,     ,       ,     .       \  .   .\\\\             
-  /     .       _--                       -\    \  .   ..  \\\\\           
- /    O .   __ /    _/                 ____|    \       .       \\\\\\\  
- /      |---  /    /---__________------    |    |---____            |  \  
-/    ,; /     |    |                       |nn  |       ---____     |  |  
-|,,,','/      /nn  |                                            ---|   |   
- ;,,;/                                                     _______/   /   
-                                                   --------_________/                                                        
+   .-=-.
+ _/     `.
+/_( 9    |.__
+  \          ``-._
+   `>    _...     `'>
+    |  .'    `-. _,'
+     \ `-.    ,' ;
+      `.  `~~'  /
+      _ 7`"..."'
+      ,'| __/
+          ,'\                                                      
 '''
-def dfs_prep(START: State):
+##########################
+#   [ SUMMARY ]
+# Starting point: GOAL, with cost = 0
+#
+# Objective: find path to START with cost >= START.current_tax
+#
+# About DFS:
+#  > expanding "node" in order of L <-; U ^ ; R ->; D v
+#  > fully ensure DFS will form a (big) counter-clockwise loop when moving from START to GOAL,
+#    thus quickly and effortlessly "cheating tax".
+#  > Strength/Weakness:
+#    +> If possible, DFS can find path EXTREMELY quickly, albeit a few exceptions in Weakness.
+#       > Limiting time to either 10 or 60 seconds yields no difference in solved/unsolved states.
+#    +> is very space friendly. Of all 28 solved states, only 2 exceeded 100 recursive iterations.
+#
+#    -> Cannot find path if there can only be a way into/out-of START node (len(START.available_moves_list()) <= 1)
+#       (under time limit and/or space/recursion limit; this does not count move-to-(0,0))
+#       > This weakness is very clear in 21/50 unsolved states, where each run iterates no less than 5000 times.
+#       > 1 state is unsolvable in all 3 algorithms we implemented and by brute-force, so it is excluded.
+#
+##########################
+
+def dfs_prep(START):
     ''' init basic vars '''
     #############################################
     # Auxiliary vars for reduced querying need
+    #DBG!: number of recursive DFS iterations
     global _START_point, _board_size, _START_tax, GOAL
-
+    if MODE_DBG: global ite; ite = 0
+    
     _board_size = START.board_size
     _START_point = START.current_pos
     _START_tax = START.current_tax
-
 
     #############################################
     # GOAL: where we start our path-finding:
     #       inherit properties from START.
     GOAL = deepcopy(START)
-    GOAL.current_pos = Coordinate(*GOAL.board_size)
+    GOAL.current_pos = Coordinate(*_board_size)
     GOAL.current_tax = 0
-    GOAL.walked_moves = START.walked_moves
-    print("GOAL:", GOAL.current_pos, ',', GOAL.current_tax)
+
+    #DBG!: info
+    if MODE_DBG: print("GOAL:", GOAL.current_pos, ',', GOAL.current_tax)
 
     # attach a visited list
     GOAL.path = list()
 
     #############################################
     # START: where we are ending
-    #DBG!: print its stats
-    print("START:", _START_point, ',', _START_tax)
+    #DBG!: info
+    if MODE_DBG: print("START:", _START_point, ',', _START_tax)
 
     dfs(GOAL)
+    #DBG!: ite, explained above
+    if MODE_DBG: print(ite)
+    
     return GOAL.path[::-1]
 
 
@@ -78,13 +91,14 @@ def dfs(cur):
         print(f"ABORT: time limit reached: {t_limit}", file = f)
         t_limit_r = True
         pass
-
     else:
-        # ABORT signal var
-        global exiting
+        # ABORT: exiting
+        global exiting, ite
         if not exiting and cur.available_moves_list() is not None:
             for _move in cur.available_moves_list():
-                # ABORT signal
+                #DBG!: iter
+                if MODE_DBG: ite += 1
+
                 if exiting: break
 
                 # temporary node
@@ -92,45 +106,55 @@ def dfs(cur):
 
                 # and move to that node
                 temp.move_on_move(_move)
-                if cur.current_pos == _START_point:
+                
+                _c = _move.coordinate_end_calc()
+                _mv = Move(
+                    _c.x,
+                    _c.y,
+                    _move.reverse_direction()
+                )
+                temp.path.append(_mv)
+                
+                if temp.current_pos == _START_point:
                     # found path
-                    if cur.current_tax >= _START_tax:
+                    if temp.current_tax >= _START_tax:
                         # found solution
                         GOAL.path = temp.path
                         exiting = True
                         break
                     
-                    GOAL.path = temp.path
-
-                _c = _move.coordinate_end_calc()
-                _move = Move(
-                    _c.x,
-                    _c.y,
-                    _move.reverse_direction()
-                )
-                temp.path.append(_move)
-
+                    GOAL.path = deepcopy(temp.path)
+                    
+                    # Prevent loss of possible solution
+                    if len(temp.available_moves_list()) <= 1: continue
+                
                 dfs(temp)    
 
 if __name__ == "__main__":
-    t_limit = 30 # seconds
+    t_limit = 10 # seconds
 
-    with open("AI_intro_project/search_algo/load_all_output/output-dfs.txt", "w") as f:
-        for _internalVar in _Utilities().load_all(
+    # Figure of (un-)solved paths
+    path = 'AI_intro_project/_s_dfs_solved_state_images/'
+
+    with open(f"AI_intro_project/search_algo/load_all_output/output-dfs-L{t_limit}.txt", "w") as f:
+        _var_utils_loadall = _Utilities().load_all(
             sizes=[(i,j) for i in range(4,9) for j in range(4,9)],
             directory='AI_intro_project/randomized_states',
             extension='state'
-        ):
+        )
+
+        for idx, _internalVar in enumerate(_var_utils_loadall):
+            # start timer
             timer = time.time()
 
-            # print basic info
+            # print basic info to output file
             print("\nboard size:",_internalVar.board_size, file = f)
             print(
                 f"start at: ({_internalVar.current_pos.x}, {_internalVar.current_pos.y})",
                 file = f
             )
             print("tax:", _internalVar.current_tax, file = f)
-
+            
             # signal DFS to ABORT: found solution
             exiting = False
 
@@ -139,7 +163,15 @@ if __name__ == "__main__":
             t_limit_r = False
 
             # print
-            print(*dfs_prep(_internalVar), sep = '\n', file = f)
+            print(*(moves:=dfs_prep(_internalVar)), sep = '\n', file = f)
+            if t_limit_r: print(f"reached time limit: {t_limit}", file = f) 
+            for mv in moves:
+                _internalVar.move_on_move(mv)
+            
+            #_internalVar.visualize()
+            _internalVar._plt_prepare()
+            plt.savefig(path + str(idx).zfill(2))
+            plt.clf()
 
             timer -= time.time()
             print(f"time: {-timer}", file = f)
