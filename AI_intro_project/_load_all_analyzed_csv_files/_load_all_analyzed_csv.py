27d50916f9f33a056c9510440c40b9b8814b7105
htnminh <nhatminh2106@gmail.com>
2021-12-24 20:53:08 +0700

    added script to analyze csv files

---
+++ AI_intro_project/_load_all_analyzed_csv_files/_load_all_analyzed_csv.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.reset_option("all")
+
+
+length_count = 81
+output_dir = r'AI_intro_project\_load_all_analyzed_csv_files'
+output_txt_path = output_dir + r'\_load_all_analyzed_csv.txt'
+
+
+with open(output_txt_path, 'w') as fo:
+    print('-' * length_count, file=fo)
+    print('DFS'.center(80), file=fo)
+    print('-' * length_count, file=fo)
+    dfs_csv_path = r'AI_intro_project\search_algo\load_all_output\output-dfs.csv'
+    df_dfs = pd.read_csv(dfs_csv_path, header=0)
+    print(df_dfs.describe(), file=fo)
+
+    print('-' * length_count, file=fo)
+    print('A*'.center(80), file=fo)
+    print('-' * length_count, file=fo)
+    astar_csv_path = r'AI_intro_project\search_algo\load_all_output\output-astar.csv'
+    df_astar = pd.read_csv(astar_csv_path, header=0)
+    print(df_astar.describe(), file=fo)
+
+print('Saved to', output_txt_path)
+
+
+# TODO: plot results in subplots using pd
